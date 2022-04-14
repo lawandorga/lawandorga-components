593e440b8587facdc814a348f231d7c9ede44a37
Daniel Mössner <github@tortuga-webdesign.de>
2022-04-14 21:04:32 +0200

    improve 2 components

--- src/components/FormInput.vue
+++ src/components/FormInput.vue
@@ -1,115 +1,121 @@
 <template>
-  <label :for="name" class="block relative">
+  <label :for="id" class="relative block">
     <div>
       <FormLabel :required="required" :label="label" />
     </div>
-    <div class="flex space-x-2 items-center" :class="{ 'mt-1': !!label }">
+    <div class="flex items-center space-x-2" :class="{ 'mt-1': !!label }">
       <div class="relative w-full">
         <input
-          :id="`form--${name}`"
-          :name="name"
+          :id="id"
+          ref="input"
+          v-model="model"
           :type="type"
           :placeholder="placeholder"
-          :value="modelValue"
           :step="step"
           :min="min"
           :required="required"
           :disabled="disabled"
           :autocomplete="autocomplete"
-          class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-1 focus-within:ring-1 focus-within:ring-lorgablue focus-within:border-lorgablue focus:ring-lorgablue focus:border-lorgablue sm:text-sm"
+          class="block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-1 focus-within:ring-1 focus-within:ring-lorgablue focus-within:border-lorgablue focus:ring-lorgablue focus:border-lorgablue sm:text-sm"
           :class="{
             'opacity-50 bg-gray-100 pointer-events-none': disabled,
           }"
-          @input="update($event)"
+          @focusout="focusoutHandler()"
         />
       </div>
-    </div>
-    <div
-      v-if="
-        type === 'datetime-local' && modelValue && !modelValue.includes('T')
-      "
-      class="mt-1 text-right text-sm text-gray-700"
-    >
-      Error: {{ modelValue }}
     </div>
     <FormHelptext :helptext="helptext" />
   </label>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import FormLabel from "./FormLabel.vue";
 import FormHelptext from "./FormHelptext.vue";
-import { defineComponent } from "vue";
+import { computed, ref, toRefs } from "vue";
 
-export default defineComponent({
-  components: {
-    FormHelptext,
-    FormLabel,
+const id = Math.random().toString();
+
+const props = defineProps({
+  label: {
+    required: true,
+    type: String,
   },
-  props: {
-    label: {
-      required: true,
-      type: String,
-    },
-    helptext: {
-      required: false,
-      default: "",
-      type: String,
-    },
-    modelValue: {
-      required: false,
-      default: "",
-      type: [String, Number, Boolean],
-    },
-    type: {
-      default: "text",
-      type: String,
-      required: false,
-    },
-    name: {
-      required: false,
-      type: String,
-      default: "",
-    },
-    placeholder: {
-      required: false,
-      default: null,
-      type: String,
-    },
-    step: {
-      required: false,
-      default: null,
-      type: Number,
-    },
-    min: {
-      required: false,
-      default: null,
-      type: Number,
-    },
-    required: {
-      required: false,
-      default: false,
-      type: Boolean,
-    },
-    disabled: {
-      required: false,
-      default: false,
-      type: Boolean,
-    },
-    autocomplete: {
-      required: false,
-      default: null,
-      type: String,
-    },
+  helptext: {
+    required: false,
+    default: "",
+    type: String,
   },
-  emits: ["update:modelValue"],
-  methods: {
-    update($event: Event) {
-      this.$emit(
-        "update:modelValue",
-        ($event.target as HTMLInputElement).value,
-      );
-    },
+  modelValue: {
+    required: false,
+    default: "",
+    type: [String, Number],
+  },
+  type: {
+    default: "text",
+    type: String,
+    required: false,
+  },
+  placeholder: {
+    required: false,
+    default: null,
+    type: String,
+  },
+  step: {
+    required: false,
+    default: null,
+    type: Number,
+  },
+  min: {
+    required: false,
+    default: null,
+    type: Number,
+  },
+  required: {
+    required: false,
+    default: false,
+    type: Boolean,
+  },
+  disabled: {
+    required: false,
+    default: false,
+    type: Boolean,
+  },
+  autocomplete: {
+    required: false,
+    default: null,
+    type: String,
   },
 });
+
+const { modelValue } = toRefs(props);
+
+const input = ref<HTMLInputElement | null>(null);
+
+const emit = defineEmits(["update:modelValue", "change:modelValue"]);
+
+const model = computed({
+  get: () => {
+    return modelValue.value;
+  },
+  set: (v) => {
+    emit("update:modelValue", v);
+  },
+});
+
+// emit change
+const focusoutHandler = () => {
+  emitChange();
+};
+
+/*
+the event change:modelValue is supposed to be triggered as soon as 
+the user is finished typing. obviously in reality it is not possible to know when this is.
+therefore this component emits this event when the focus or the route 
+changes and the value of the input is not in sync with the modelvalue prop.
+*/
+const emitChange = () => {
+  if (input.value === null) return;
+  const value = input.value.value;
+  if (value !== model.value) emit("change:modelValue", value);
+};
 </script>
--- src/components/FormTextarea.vue
+++ src/components/FormTextarea.vue
@@ -1,68 +1,87 @@
 <template>
-  <label class="block">
+  <label class="block" :for="id">
     <FormLabel :required="required" :label="label" />
     <textarea
-      :id="`textarea--${name}`"
-      class="mt-1 appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-lorgablue focus:border-lorgablue sm:text-sm"
-      :value="modelValue.toString()"
+      :id="id"
+      ref="input"
+      v-model="model"
+      class="block w-full px-3 py-2 mt-1 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-1 focus:ring-lorgablue focus:border-lorgablue sm:text-sm"
       :placeholder="placeholder"
-      :name="name"
       :required="required"
-      @input="update($event)"
+      @focusout="focusoutHandler()"
     ></textarea>
     <FormHelptext :helptext="helptext" />
   </label>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import FormLabel from "./FormLabel.vue";
 import FormHelptext from "./FormHelptext.vue";
-import { defineComponent } from "vue";
+import { computed, ref, toRefs } from "vue";
 
-export default defineComponent({
-  components: {
-    FormLabel,
-    FormHelptext,
+const id = Math.random().toString();
+
+const props = defineProps({
+  label: {
+    required: true,
+    type: String,
   },
-  props: {
-    label: {
-      required: true,
-      type: String,
-    },
-    helptext: {
-      required: false,
-      default: "",
-      type: String,
-    },
-    modelValue: {
-      required: false,
-      default: "",
-      type: [String, Boolean, Number],
-    },
-    name: {
-      required: false,
-      type: String,
-      default: "",
-    },
-    required: {
-      required: false,
-      default: false,
-      type: Boolean,
-    },
-    placeholder: {
-      required: false,
-      default: "",
-      type: String,
-    },
+  helptext: {
+    required: false,
+    default: "",
+    type: String,
   },
-  emits: ["update:modelValue"],
-  methods: {
-    update($event: Event) {
-      this.$emit(
-        "update:modelValue",
-        ($event.target as HTMLInputElement).value,
-      );
-    },
+  modelValue: {
+    required: false,
+    default: "",
+    type: String,
+  },
+  name: {
+    required: false,
+    type: String,
+    default: "",
+  },
+  required: {
+    required: false,
+    default: false,
+    type: Boolean,
+  },
+  placeholder: {
+    required: false,
+    default: "",
+    type: String,
   },
 });
+
+const { modelValue } = toRefs(props);
+
+const input = ref<HTMLInputElement | null>(null);
+
+const emit = defineEmits(["update:modelValue", "change:modelValue"]);
+
+const model = computed({
+  get: () => {
+    return modelValue.value;
+  },
+  set: (v) => {
+    emit("update:modelValue", v);
+  },
+});
+
+// emit change
+const focusoutHandler = () => {
+  emitChange();
+};
+
+/*
+the event change:modelValue is supposed to be triggered as soon as 
+the user is finished typing. obviously in reality it is not possible to know when this is.
+therefore this component emits this event when the focus or the route 
+changes and the value of the input is not in sync with the modelvalue prop.
+*/
+const emitChange = () => {
+  if (input.value === null) return;
+  const value = input.value.value;
+  if (value !== model.value) emit("change:modelValue", value);
+};
 </script>
